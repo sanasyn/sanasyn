23998b5adfbaf674d01d553f6081fe0cd1dc82eb
Whitney Wong <whitney.wong@utexas.edu>
2018-05-12 20:18:03 -0500

    initial commit for About Us page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LandingPage from './LandingPage';
 import Main from './Main';
+import About from './AboutUs'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import { Image } from 'react-bootstrap';
@@ -11,7 +12,7 @@ class App extends Component {
     return (
     <BrowserRouter>
       <MuiThemeProvider>
-        <div>
+        <div className='mainContainer'>
           <header className='header'>
             <Link to='/' style={{textDecoration: "none"}}>
               <span className='logo'>S&#423;</span>
@@ -22,8 +23,13 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={LandingPage} />
               <Route exact path="/quiz" component={Main} />
+              <Route exact path="/about" component={About} />
             </Switch>
           </div>
+          <footer className='footer'>
+            <Link className='footerLogo' to='/' style={{textDecoration: "none"}}>SanaSyn</Link> | 
+            <Link className="aboutLink" to='/about' style={{textDecoration: "none"}}> About Us</Link>
+          </footer>
         </div>
       </MuiThemeProvider>
     </BrowserRouter>
