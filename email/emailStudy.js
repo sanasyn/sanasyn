da21d1cef2dbfe7ad19fe008ad4849bf7b03912e
Whitney Wong <whitney.wong@utexas.edu>
2018-09-04 20:53:52 -0500

    Initial commit

--- email/emailStudy.js
+++ email/emailStudy.js
@@ -28,3 +28,4 @@ transporter.sendMail(mailOptions, (error, info) => {
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 });
+
