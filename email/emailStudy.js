47d4be02a3c76e3ce72f32b91a9a443fc85b1024
Whitney Wong <whitney.wong@utexas.edu>
2018-09-12 21:51:20 -0500

    Basic function working.

--- email/emailStudy.js
+++ email/emailStudy.js
@@ -28,4 +28,3 @@ transporter.sendMail(mailOptions, (error, info) => {
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 });
-
